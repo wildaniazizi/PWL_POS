c9c60ec3fa17f0d06c31eee70ae40018f4ccf774
Mochamad Wildani Azizi <wwilldanazizi@gmail.com>
2024-03-15 03:21:24 +0700

    Practicum-2.3

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -45,9 +45,12 @@ class UserController extends Controller
         });
 
         $user = UserModel::findOrFail(1);
+
+        $user = UserModel::where('username', 'manager')->firstOrFail();
         */
 
-        $user = UserModel::where('username', 'manager9')->firstOrFail();
-        return view('user', ['data' => $user]);
+        $user = UserModel::where('level_id', 2)->count();
+        return view('user', ['user' => $user]);
+
     }
 }
--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -8,18 +8,13 @@
         <table border="1" cellpadding="2" cellspacing="0">
             <tr>
                 <tr>
-                    <th>ID</th>
-                    <th>Username</th>
-                    <th>Nama</th>
-                    <th>ID Level Pengguna</th>
+                    <th>Jumlah Pengguna</th>
                 </tr>
                 <tr>
-                    <th>{{$data->user_id}}</th>
-                    <th>{{$data->username}}</th>
-                    <th>{{$data->nama}}</th>
-                    <th>{{$data->level_id}}</th>
+                    <th>{{$user}}</th>
                 </tr>
             </tr>
         </table>
+        
     </body>
 </html>
