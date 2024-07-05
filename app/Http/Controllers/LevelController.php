e12649f67ade178cf73fcab82d346a57da1f35f4
Mochamad Wildani Azizi <wwilldanazizi@gmail.com>
2024-07-05 14:12:12 +0700

    jobsheet-10

--- app/Http/Controllers/LevelController.php
+++ app/Http/Controllers/LevelController.php
@@ -1,164 +1,36 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api;
 
+use App\Http\Controllers\Controller;
 use App\Models\LevelModel;
 use Illuminate\Http\Request;
-use Yajra\DataTables\Facades\DataTables;
 
 class LevelController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        $breadcrumb = (object) [
-            'title' => 'Daftar Level',
-            'list' => ['Home', 'Level']
-        ];
-
-        $page = (object) [
-            'title' => 'Daftar level yang terdaftar dalam sistem'
-        ];
-
-        $activeMenu = 'level';
-        $level = LevelModel::all();
-
-        return view('level.index', ['breadcrumb' => $breadcrumb, 'page' => $page, 'activeMenu' => $activeMenu, 'level' => $level]);
+    public function index() {
+        return LevelModel::all();
     }
 
-    public function list(Request $request) {
-        $levels = LevelModel::select('level_id', 'level_kode', 'level_nama');
-
-        return DataTables::of($levels)
-            ->addIndexColumn()
-            ->addColumn('action', function ($level) {
-                $btn = '<a href="'.url('/level/' . $level->level_id).'" class="btn btn-info btn-sm">Detail</a> ';
-                $btn .= '<a href="'.url('/level/' . $level->level_id . '/edit').'" class="btn btn-warning btn-sm">Edit</a> ';
-                $btn .= '<form class="d-inline-block" method="POST" action="'.url('/level/'.$level->level_id).'">'
-                    . csrf_field() . method_field('DELETE') .
-                    '<button type="submit" class="btn btn-danger btn-sm" onclick="return confirm(\'Are you sure to delete this data?\');">Delete</button></form>';
-                return $btn;
-            })
-            ->rawColumns(['action'])
-            ->make(true);
+    public function store(Request $request) {
+        $level = LevelModel::create($request->all());
+        return response()->json($level, 201);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        $breadcrumb = (object) [
-            'title' => 'Tambah Level',
-            'list' => ['Home', 'Level', 'Tambah Level']
-        ];
-
-        $page = (object) [
-            'title' => 'Tambah level baru'
-        ];
-
-        $activeMenu = 'level';
-
-        return view('level.create', ['breadcrumb' => $breadcrumb, 'page' => $page, 'activeMenu' => $activeMenu]);
+    public function show(LevelModel $level) {
+        return LevelModel::find($level);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        $request->validate([
-            'level_kode' => 'required',
-            'level_nama' => 'required'
+    public function update(Request $request, LevelModel $level) {
+        $level->update($request->all());
+        return LevelModel::find($level);
+    }
+
+    public function destroy(LevelModel $level) {
+        $level->delete();
+        return response()->json([
+            'success' => true,
+            'message' => 'Data berhasil dihapus!'
         ]);
-
-        LevelModel::create([
-            'level_kode' => $request->level_kode,
-            'level_nama' => $request->level_nama
-        
-        ]);
-
-        return redirect('/level')->with('success', 'Level berhasil ditambahkan');
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        $level = LevelModel::find($id);
-
-        $breadcrumb = (object) [
-            'title' => 'Detail Level',
-            'list' => ['Home', 'Level', 'Detail']
-        ];
-
-        $page = (object) [
-            'title' => 'Detail level'
-        ];
-
-        $activeMenu = 'level';
-
-        return view('level.show', ['breadcrumb' => $breadcrumb, 'page' => $page, 'activeMenu' => $activeMenu, 'level' => $level]);
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        $level = LevelModel::find($id);
-
-        $breadcrumb = (object) [
-            'title' => 'Edit Level',
-            'list' => ['Home', 'Level', 'Edit']
-        ];
-
-        $page = (object) [
-            'title' => 'Edit level'
-        ];
-
-        $activeMenu = 'level';
-
-        return view('level.edit', ['breadcrumb' => $breadcrumb, 'page' => $page, 'activeMenu' => $activeMenu, 'level' => $level]);
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        $request->validate([
-            'level_kode' => 'required',
-            'level_nama' => 'required'
-        ]);
-
-        LevelModel::find($id)->update([
-            'level_kode' => $request->level_kode,
-            'level_nama' => $request->level_nama
-        ]);
-
-        return redirect('/level')->with('success', 'Level berhasil diubah');
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        $check = LevelModel::find($id);
-
-        if (!$check) {
-            return redirect('/level')->with('error', 'Data level tidak ditemukan');
-        }
-
-        try {
-            $check->delete();
-            return redirect('/level')->with('success', 'Data level berhasil dihapus');
-        } catch (\Illuminate\Database\QueryException $e) {
-            return redirect('/level')->with('error', 'Data level gagal dihapus');
-        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\Api\LevelController;
+use App\Http\Controllers\Api\LoginController;
+use App\Http\Controllers\Api\LogoutController;
+use App\Http\Controllers\Api\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +18,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::post('/register', RegisterController::class)->name('register');
+Route::post('/login', LoginController::class)->name('login');
+Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::post('/logout', LogoutController::class)->name('logout');
+
+Route::get('levels', [LevelController::class, 'index']);
+Route::post('levels', [LevelController::class, 'store']);
+Route::get('levels/{level}', [LevelController::class, 'show']);
+Route::put('levels/{level}', [LevelController::class, 'update']);
+Route::delete('levels/{level}', [LevelController::class, 'destroy']);
