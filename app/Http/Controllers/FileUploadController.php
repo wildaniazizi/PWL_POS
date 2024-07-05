2fda102e2eb5cd0c14832bc503dd7bcb47490e24
Mochamad Wildani Azizi <wwilldanazizi@gmail.com>
2024-07-05 15:48:10 +0700

    JB12-E

--- app/Http/Controllers/FileUploadController.php
+++ app/Http/Controllers/FileUploadController.php
@@ -39,8 +39,12 @@ class FileUploadController extends Controller
             $extFile=$request->berkas->getClientOriginalName();
             $namaFile='web-'.time().".".$extFile;
             // $path = $request->berkas->store('uploads');
-            $path = $request->berkas->storeAs('uploads', $namaFile);
-            echo "proses upload berhasil, data disimpan pada: $path";
+            $path = $request->berkas->storeAs('public',$namaFile );
+
+            $pathBaru=asset('storage/'.$namaFile);
+            echo "proses upload berhasil, file berada di: $path";
+            echo "<br>";
+            echo "Tampilkan link:<a href='$pathBaru'>$pathBaru</a>";
             // echo $request->berkas->getClientOriginalName()."<br>lolos validasi";
     }
 }
