aaa04b8d106bbd791eecf6808a75f5a6241feae5
Mochamad Wildani Azizi <wwilldanazizi@gmail.com>
2024-07-05 17:38:42 +0700

    file-upload assignment

--- app/Http/Controllers/FileUploadController.php
+++ app/Http/Controllers/FileUploadController.php
@@ -1,5 +1,6 @@
 <?php
 
+namespace App\Http\Controllers\Api;
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
@@ -15,16 +16,26 @@ class FileUploadController extends Controller
         $request->validate([
             'berkas'=>'required|file|image|max:500',]);
             $extFile=$request->berkas->getClientOriginalName();
-            $namaFile='web-'.time().".".$extFile;
-            // $path = $request->berkas->store('uploads');
-            $path = $request->berkas->storeAs('gambar',$namaFile );
-            $path=str_replace("\\","//", $path);
-            echo "Variabel path berisi: $path <br>";
+            // $namaFile='web-'.time().".".$extFile;
 
-            $pathBaru=asset('gambar/'.$namaFile);
-            echo "proses upload berhasil, file berada di: $path";
-            echo "<br>";
-            echo "Tampilkan link:<a href='$pathBaru'>$pathBaru</a>";
-            // echo $request->berkas->getClientOriginalName()."<br>lolos validasi";
+            $namaFile = $request->namaBerkas;
+
+            $path = $request->berkas->move('gambar', '$namaFile');
+            $pathBaru=asset('gambar/'.$namaFile); 
+
+            echo "Gambar berhasil di upload ke <a href='$pathBaru'>$namaFile</a>";
+            echo "<br> <br>";
+            echo "<img src='$path' alt='$namaFile' width='50%' height='50%'>";
+
+            // // $path = $request->berkas->store('uploads');
+            // $path = $request->berkas->storeAs('gambar',$namaFile );
+            // $path=str_replace("\\","//", $path);
+            // echo "Variabel path berisi: $path <br>";
+
+            // $pathBaru=asset('gambar/'.$namaFile);
+            // echo "proses upload berhasil, file berada di: $path";
+            // echo "<br>";
+            // echo "Tampilkan link:<a href='$pathBaru'>$pathBaru</a>";
+            // // echo $request->berkas->getClientOriginalName()."<br>lolos validasi";
     }
 }
--- resources/views/file-upload.blade.php
+++ resources/views/file-upload.blade.php
@@ -14,6 +14,8 @@
         <form action="{{ url('/file-upload') }}" method="POST" enctype="multipart/form-data">
             @csrf
             <div class="mb-3">
+                <label for="namaBerkas" class="form-label">Nama Gambar</label>
+                <input type="text" class="form-control" id="namaBerkas" name="namaBerkas">
                 <label for="berkas" class="form-label">Gambar Profile</label>
                 <input type="file" class="form-control" id="berkas" name="berkas">
                 @error('berkas')
