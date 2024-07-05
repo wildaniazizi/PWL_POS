8d35397baa49dfea771d92f082fec0b86f909df9
Mochamad Wildani Azizi <wwilldanazizi@gmail.com>
2024-07-05 15:47:29 +0700

    JB12-D

--- app/Http/Controllers/FileUploadController.php
+++ app/Http/Controllers/FileUploadController.php
@@ -36,6 +36,11 @@ class FileUploadController extends Controller
 
         $request->validate([
             'berkas'=>'required|file|image|max:500',]);
-        echo $request->berkas->getClientOriginalName()."<br>lolos validasi";
+            $extFile=$request->berkas->getClientOriginalName();
+            $namaFile='web-'.time().".".$extFile;
+            // $path = $request->berkas->store('uploads');
+            $path = $request->berkas->storeAs('uploads', $namaFile);
+            echo "proses upload berhasil, data disimpan pada: $path";
+            // echo $request->berkas->getClientOriginalName()."<br>lolos validasi";
     }
 }
